da00441416eb052c149667b2d59b2b76aa229360
oguzhanuyanik-sr <oguzhanuyanik.sr@gmail.com>
2023-09-24 14:17:28 +0300

    css fix

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -6,9 +6,9 @@ const Card = () => {
   return (
     <div className='rounded-[10px] overflow-hidden max-w-[343px] lg:max-w-[600px] bg-white flex flex-col lg:flex-row'>
       <div>
-        <img className='lg:hidden h-full' src={ProductMobile} alt='Product' />
+        <img className='lg:hidden' src={ProductMobile} alt='Product' />
         <img
-          className='hidden lg:block h-full'
+          className='h-full hidden lg:block'
           src={ProductDesktop}
           alt='Product'
         />
